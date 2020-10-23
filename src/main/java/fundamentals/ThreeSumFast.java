3e365de34538c206eb4083696f94482266a2a20f
Yi Shen <yshensh@gmail.com>
2020-10-22 22:01:37 -0400

    use binary search to implement fast 2sum and 3sum

--- src/main/java/fundamentals/ThreeSumFast.java
+++ src/main/java/fundamentals/ThreeSumFast.java
@@ -1,4 +1,26 @@
 package fundamentals;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 public class ThreeSumFast {
+    public static int count(int[] a) {
+        int n = a.length;
+        int counter = 0;
+        Arrays.sort(a);
+        for (int i=0; i < n; i++)
+            for (int j=i; j < n; j++) {
+                int k = Arrays.binarySearch(a, -a[i]-a[j]);
+                if (k > j) counter++;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] a = in.readAllInts();
+        StdOut.println(count(a));
+    }
 }
--- src/main/java/fundamentals/TwoSumFast.java
+++ src/main/java/fundamentals/TwoSumFast.java
@@ -1,4 +1,25 @@
 package fundamentals;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Arrays;
+
 public class TwoSumFast {
+    public static int count(int[] a) {
+        int n = a.length;
+        int counter = 0;
+        Arrays.sort(a);
+        for (int i = 0; i < n; i++) {
+            int j = Arrays.binarySearch(a, -a[i]);
+            if (j > i) counter++;
+        }
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        int[] a = in.readAllInts();
+        StdOut.println(count(a));
+    }
 }
