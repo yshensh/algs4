3fae5a827839aca26058ad1fa2cd511262f891cc
Yi Shen <yshensh@gmail.com>
2021-01-11 13:59:49 -0500

    merge sort

--- src/main/java/sorting/MergeBU.java
+++ src/main/java/sorting/MergeBU.java
@@ -6,46 +6,37 @@ package sorting;
  *
  */
 public class MergeBU {
-    // auxiliary array for merges
-    private static Comparable[] aux;
-
     /**
      * Rearranges the array in ascending order, using the natural order.
      * @param a the array to be sorted
      */
     public static void sort(Comparable[] a) {
-        aux = new Comparable[a.length];
-        sort(a, 0, a.length - 1);
-    }
-
-    // mergesort a[lo..hi] using auxiliary array aux[lo..hi]
-    private static void sort(Comparable[] a, int lo, int hi) {
-        int mid = lo + (hi - lo)/2;
-        // merge results
-        merge(a, lo, mid, hi);
+        int n = a.length;
+        Comparable[] aux = new Comparable[n];
+        for (int len = 1; len < n; len *= 2) {
+            for (int lo = 0; lo < n-len; lo += len+len) {
+                int mid  = lo+len-1;
+                int hi = Math.min(lo+len+len-1, n-1);
+                merge(a, aux, lo, mid, hi);
+            }
+        }
     }
 
     // stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
-    private static void merge(Comparable[] a, int lo, int mid, int hi) {
-        // merge a[lo..mid] with a[mid+1..hi]
-        int i = lo, j = mid + 1;
-        // copy a[lo..hi] to aux[lo..hi]
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+
+        // copy to aux[]
         for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
         }
+
         // merge back to a[lo..hi]
+        int i = lo, j = mid+1;
         for (int k = lo; k <= hi; k++) {
-            // condition 1: left half exhausted (take from the right)
-            if (i > mid) a[k] = aux[j++];
-
-                // condition 2: right half exhausted (take from the left)
-            else if (j > hi) a[k] = aux[i++];
-
-                // condition 3: current key on the right less than current key on left (take from the right)
+            if      (i > mid)              a[k] = aux[j++];  // this copying is unnecessary
+            else if (j > hi)               a[k] = aux[i++];
             else if (less(aux[j], aux[i])) a[k] = aux[j++];
-
-                // condition 4: current key on right greater than or equal to current key on left (take from the left)
-            else a[k] = aux[i++];
+            else                           a[k] = aux[i++];
         }
     }
 
--- src/test/java/sorting/MergeTest.java
+++ src/test/java/sorting/MergeTest.java
@@ -8,13 +8,23 @@ import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MergeTDTest {
+class MergeTest {
     @Test
-    public void testSort() {
+    public void MergTDSort() {
         In in = new In("src/test/resources/tiny.txt");
         String[] actual = in.readAllStrings();
         String[] expected = Arrays.copyOf(actual, actual.length);
         MergeTD.sort(actual);
+        Arrays.sort(expected);
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void MergBUSort() {
+        In in = new In("src/test/resources/tiny.txt");
+        String[] actual = in.readAllStrings();
+        String[] expected = Arrays.copyOf(actual, actual.length);
+        MergeBU.sort(actual);
         Arrays.sort(expected);
         assertArrayEquals(expected, actual);
     }
