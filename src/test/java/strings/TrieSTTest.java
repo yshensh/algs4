8abc2c1b366302d3b33a0ea1098c48f7a6014224
Yi Shen <yshensh@gmail.com>
2021-02-12 17:49:15 -0500

    trie

---
+++ src/test/java/strings/TrieSTTest.java
@@ -0,0 +1,43 @@
+package strings;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.jupiter.api.Test;
+
+
+class TrieSTTest {
+    @Test
+    public void testSort() {
+        In in = new In("src/test/resources/shellsST.txt");
+        // build symbol table from standard input
+        TrieST<Integer> st = new TrieST<Integer>();
+        for (int i = 0; !in.isEmpty(); i++) {
+            String key = in.readString();
+            st.put(key, i);
+        }
+        // print results
+        if (st.size() < 100) {
+            StdOut.println("keys(\"\"):");
+            for (String key : st.keys()) {
+                StdOut.println(key + " " + st.get(key));
+            }
+            StdOut.println();
+        }
+        StdOut.println("longestPrefixOf(\"shellsort\"):");
+        StdOut.println(st.longestPrefixOf("shellsort"));
+        StdOut.println();
+
+        StdOut.println("longestPrefixOf(\"quicksort\"):");
+        StdOut.println(st.longestPrefixOf("quicksort"));
+        StdOut.println();
+
+        StdOut.println("keysWithPrefix(\"shor\"):");
+        for (String s : st.keysWithPrefix("shor"))
+            StdOut.println(s);
+        StdOut.println();
+
+        StdOut.println("keysThatMatch(\".he.l.\"):");
+        for (String s : st.keysThatMatch(".he.l."))
+            StdOut.println(s);
+    }
+}
